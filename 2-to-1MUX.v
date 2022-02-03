f850ab69615065058350f17b584eb4001aef6bcf
SAM MARLEY <60399540+smarley19@users.noreply.github.com>
2022-02-03 16:37:23 -0500

    adding files

---
+++ 2-to-1MUX.v
@@ -0,0 +1,13 @@
+module 2_to_1_MUX(
+	input [31:0] In0,
+	input [31:0] In1,
+	input Signal,
+	output [31:0] Out
+	);
+	always @ (In0, In1)
+	case(Signal)
+	0: Out = In0;
+	1: Out = In1;
+	default: Out = 32b'x
+	endcase
+endmodule
