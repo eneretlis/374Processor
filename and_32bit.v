1754c2e0676cd6381a76641eb4bfcf6e917df423
eneretlis <18emn2@queensu.ca>
2022-02-03 20:51:19 -0500

    added reg to add

--- and_32bit.v
+++ and_32bit.v
@@ -1,10 +1,10 @@
 module and_32bit(
-	input wire [31:0] Ra,
-	input wire [31:0] Rb,
-	output wire [31:0] Rc
+	input [31:0] Ra,
+	input [31:0] Rb,
+	output reg [31:0] Rc
 	);
 
 	
-	assign Rc = Ra&&Rb;
+	assign Rc <= Ra&&Rb;
 
 endmodule 
