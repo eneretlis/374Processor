c6e20b4c7aaa594df350b52e0417db08e79cb0f6
eneretlis <18emn2@queensu.ca>
2022-02-07 14:41:56 -0500

    changed register naming

--- and_32bit.v
+++ and_32bit.v
@@ -1,10 +1,10 @@
 module and_32bit(
-	input [31:0] Ra,
 	input [31:0] Rb,
-	output reg [31:0] Rc
+	input [31:0] Rc,
+	output reg [31:0] Ra
 	);
 
 	
-	assign Rc <= Ra&&Rb;
+	assign Ra <= Rb&&Rc;
 
 endmodule 
