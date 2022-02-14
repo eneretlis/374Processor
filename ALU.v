01b8c562854cbd4244ff9bfdc0c467575790f576
eneretlis <18emn2@queensu.ca>
2022-02-14 14:55:53 -0500

    useless changes

--- ALU.v
+++ ALU.v
@@ -8,53 +8,65 @@ module ALU(
 		RotateRight = 5'b00111, RotateLeft = 5'b01000, And = 5'b01001, Or = 5'b01010, Multiply = 5'b01110, 
 		Divide = 5'b01111, Negate = 5'b10000, Not = 5'b10001;
 	always@(opcode)
-	begin
-		case(opcode)
-			Addition: begin
-								Adding add_rca_32(ZLO,Ra,Rb);
-								assign ZHI = 32'b0;
-							end
-			Subtraction : begin
-								Subbing subtract32(Ra,Rb,ZLO);
-								assign ZHI = 32'b0;
-							end
-			ShiftRight : begin
-								Shr shift_right(Ra,Rb,ZLO);
-								assign ZHI = 32'b0;
-							end
-			ShiftLeft : begin
-								Shl shift_left(Ra,Rb,ZLO);
-								assign ZHI = 32'b0;
-							end
-			RotateRight : begin
-								rightrotate ror(Ra,Rb,ZLO);
-								assign ZHI = 32'b0;
-							end
-			RotateLeft : begin
-								leftrotate rol(Ra,Rb,ZLO);
-								assign ZHI = 32'b0;
-							end
-			And : begin
-								anding and_32bit(Ra,Rb,ZLO);
-								ZHI = 32'b0;
+		begin
+			case(opcode)
+				Addition: 
+								begin
+									Adding add_rca_32(ZLO,Ra,Rb);
+									assign ZHI = 32'b0;
 								end
-			Or : begin
-								oring or_32bit(Ra,Rb,ZLO);
-								ZHI = 32'b0;
+				Subtraction : 
+								begin
+									Subbing subtract32(Ra,Rb,ZLO);
+									assign ZHI = 32'b0;
 								end
-			Multiply: begin
-								multiplying multiply_booth(Ra,Rb,ZHI,ZLO);
+				ShiftRight : 
+								begin
+									Shr shift_right(Ra,Rb,ZLO);
+									assign ZHI = 32'b0;
 								end
-			Divide: begin
-								dividing divide_32(Ra,Rb,ZHI,ZLO);
+				ShiftLeft : 
+								begin
+									Shl shift_left(Ra,Rb,ZLO);
+									assign ZHI = 32'b0;
 								end
-			Negate: begin
-								negating negate_32(Ra,ZLO);
-								ZHI = 32'b0;
+				RotateRight :
+								begin
+									rightrotate ror(Ra,Rb,ZLO);
+									assign ZHI = 32'b0;
 								end
-			Not: begin
-								notting not_32(Ra,ZLO);
-								ZHI = 32'b0;
+				RotateLeft : 
+								begin
+									leftrotate rol(Ra,Rb,ZLO);
+									ZHI <= 32'b0;
+								end
+				And : 
+								begin
+									anding and_32bit(Ra,Rb,ZLO);
+									ZHI = 32'b0;
+								end
+				Or : 
+								begin
+									or_32bit(Ra,Rb,ZLO);
+									oring ZHI = 32'b0;
+								end
+				Multiply: 
+								begin
+									multiplying multiply_booth(Ra,Rb,ZHI,ZLO);
+								end
+				Divide: 
+								begin
+									dividing divide_32(Ra,Rb,ZHI,ZLO);
+								end
+				Negate: 
+								begin
+									negating negate_32(Ra,ZLO);
+									ZHI = 32'b0;
+								end
+				Not: 
+								begin
+									notting not_32(Ra,ZLO);
+									ZHI = 32'b0;
 								end
 		endcase
 	end
