42abb5384fd579c48f552efb61ada37065cde0c4
SAM MARLEY <60399540+smarley19@users.noreply.github.com>
2022-03-25 16:03:00 -0400

    updates to the control unit

--- control_unit.v
+++ control_unit.v
@@ -28,7 +28,9 @@ module control_unit (
 				jr3 = 8'b0111101,
 				jal3 = 8'b0111110, jal4= 8'b0111111,
 				mfhi3 = 8'b01000000, mflo3 = 8'b01000001,
-				in3 = 8'b01000010, out3 = 8'b01000011;
+				in3 = 8'b01000010, out3 = 8'b01000011
+				and3 = 8'b01000100, and4 = 8'b01000101, and5 = 8'b01000110,
+				or3 = 8'b01000111, or4 = 8'b01001000, or5 = 8'b1001001;
 	reg [3:0] Present_state = Reset_state; // adjust the bit pattern based on the number of states
 	always @(posedge Clock, posedge Reset) // finite state machine; if clock or reset rising-edge
 		begin
@@ -79,14 +81,36 @@ module control_unit (
 				st4 : Present_state = st5;
 				st5 : Present_state = st6;
 				st6 : Present_state = st7;
+				addi3 : Present_state = addi4;
+				addi4 : Present_state = addi5;
+				andi3 : Present_state = andi4;
+				andi4 : Present_state = andi5;
+				ori3 : Present_state = ori4;
+				ori4 : Present_state = ori5;
+				br3 : Present_state = br4;
+				br4 : Present_state = br5;
+				br5 : Present_state = br6;
+				br6 : Present_state = br7;
+				jal3 : Present_state = jal4;
+				and3 : Present_state = and4;
+				and4 : Present_state = and5;
+				or3 : Present_state = or4;
+				or4 : Present_state = or5;
 			endcase
 		end
 	always @(Present_state) // do the job for each state
 		begin
 			case (Present_state) // assert the required signals in each state
 				Reset_state: begin
-					Gra <= 0; Grb <= 0; Grc <= 0; Yin <= 0; // initialize the signals
-					⁞
+					Gra <= 0; Grb <= 0; Grc <= 0; Yin <= 0;
+					Gra <=0;Grb <=0; Grc <=0; Rin <=0; BAout <=0; Rout <=0;//initialize the signals
+					MARIn <=0; PCIn <=0; MDRIn <=0;IRIn <=0; YIn <=0; IncPC <=0; 
+					HiIn <=0; LoIn <=0; CIn <=0;InIn <=0; OutIn <=0; ZIn <=0; CONIn <=0;
+					PCout <=0; Zlowout <=0; Zhighout <=0; MDRout <=0; Cout <=0;
+					In_Portout <=0; LOout <=0; HIout <=0;
+					Read <=0; Write <=0; Clear,
+					ADD <=0; AND <=0; SUB <=0; DIV <=0; SHR <=0; SHL <=0; 
+					ROR <=0; ROL <=0; OR <=0; MUL <=0; NEG <=0; NOT <=0;
 				end
 				fetch0: begin
 					PCout <= 1; // see if you need to de-assert these signals
@@ -94,11 +118,178 @@ module control_unit (
 					IncPC <= 1;
 					Zin <= 0;
 				end
+				fetch1: begin
+					Zlowout <=1; PCIn <=1; 
+					MDRIn <=1; read <= 1; 
+				end
+				fetch2: begin
+				end
 				add3: begin
 					Grb <= 1; Rout <= 1;
 					Yin <= 0;
 				end
-				⁞
+				add4: begin
+				end
+				add5: begin
+				end
+				
+				sub3: begin
+				end
+				sub4: begin
+				end
+				sub5: begin
+				end
+				
+				mul3: begin
+				end
+				mul4: begin
+				end
+				mul5: begin
+				end
+				mul6: begin
+				end
+				
+				div3: begin
+				end
+				div4: begin
+				end
+				div5: begin
+				end
+				div6: begin
+				end
+				
+				shr3: begin
+				end
+				shr4: begin
+				end
+				shr5: begin
+				end
+				
+				shl3: begin
+				end
+				shl4: begin
+				end
+				shl5: begin
+				end
+				
+				ror3: begin
+				end
+				ror4: begin
+				end
+				ror5: begin
+				end
+				
+				rol3: begin
+				end
+				rol4: begin
+				end
+				rol5: begin
+				end
+				
+				neg3: begin
+				end
+				neg4: begin
+				end
+				
+				not3: begin
+				end
+				not4: begin
+				end
+				
+				ld3: begin
+				end
+				ld4: begin
+				end
+				ld5: begin
+				end
+				ld6: begin
+				end
+				ld7: begin
+				end
+				
+				ldi3: begin
+				end
+				ldi4: begin
+				end
+				ldi5: begin
+				end
+				
+				st3: begin
+				end
+				st4: begin
+				end
+				st5: begin
+				end
+				st6: begin
+				end
+				st7: begin
+				end
+				
+				addi3: begin
+				end
+				addi4: begin
+				end
+				addi5: begin
+				end
+				
+				andi3: begin
+				end
+				andi4: begin
+				end
+				andi5: begin
+				end
+				
+				ori3: begin
+				end
+				ori4: begin
+				end
+				ori5: begin
+				end
+				
+				br3: begin
+				end
+				br4: begin
+				end
+				br5: begin
+				end
+				br6: begin
+				end
+				br7: begin
+				end
+				
+				jr3: begin
+				end
+				
+				jal3: begin
+				end
+				jal4: begin
+				end
+				
+				mfhi3: begin
+				end
+				
+				mflo3: begin
+				end
+				
+				in3: begin
+				end
+				
+				out3: begin
+				end
+				
+				and3: begin
+				end
+				and4: begin
+				end
+				and5: begin
+				end
+				
+				or3: begin
+				end
+				or4: begin
+				end
+				or5: begin
+				end
 			endcase
 		end
 endmodule
